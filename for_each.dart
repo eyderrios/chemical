757524e665d793e340393b7a8e85bfaa75700ec1
Eyder Rios <eyder.rios@gmail.com>
2022-08-15 17:37:08 -0300

    Incluido teste do getter formula no arquivo main.dart

--- for_each.dart
+++ for_each.dart
@@ -1,29 +1,31 @@
+/*
+ * Exemplos de implementação do forEach()
+ */
 
 typedef MyCallback = void Function(int);
 
 class MyClass {
-  final _myList = [1,2,3,4,5];
+  final _myList = [1, 2, 3, 4, 5];
 
   void forEach(void Function(int) action) {
-    for(var data in _myList) {
+    for (var data in _myList) {
       action(data);
     }
-    print('-'*10);
+    print('-' * 10);
   }
 
   void forEach2(void action(int _)) {
-    for(var data in _myList) {
+    for (var data in _myList) {
       action(data);
     }
-    print('-'*10);
+    print('-' * 10);
   }
 
   void forEach3(MyCallback action) {
-    for(var data in _myList) {
+    for (var data in _myList) {
       action(data);
     }
   }
-
 }
 
 void myAction(int elem) {
@@ -32,18 +34,16 @@ void myAction(int elem) {
 
 void main() {
   final myClass = MyClass();
-  
+
   myClass.forEach(myAction);
-  
-  myClass.forEach( (int elem) { 
-    print('V2: $elem'); 
+
+  myClass.forEach((int elem) {
+    print('V2: $elem');
   });
 
-  myClass.forEach( (elem) { 
-    print('V3: $elem'); 
+  myClass.forEach((elem) {
+    print('V3: $elem');
   });
-  
-  myClass.forEach( (elem) => print('V4: $elem') );
-  
+
+  myClass.forEach((elem) => print('V4: $elem'));
 }
-
